dc493f562a68bccc9b571c6f2d81f83767fba136
vaarigupta <vaarigupta24@gmail.com>
2019-03-03 00:56:57 +0530

    cards added

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. 
-By sending request at '/' , it renders the index.hbs page along with a hook named title 
+/* GET home page.
+By sending request at '/' , it renders the index.hbs page along with a hook named title
 which says that wherever the title used in the views folder , change it to shopping cart
 
 */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Shopping Cart' });
+  res.render('./shop/index', { title: 'Shopping Cart' });
 });
 
 module.exports = router;
